bdd4305afea0e85096ebe267f777263c515c64a6
unknown <bellecour@D24-45.bellecour.local>
2014-11-22 09:40:58 +0100

    Level generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour
+{
+    // Nombre de blovks à générer d'avance
+    public int MeshesToGenerate;
+
+
+    #region Comportements Unity
+    void Start()
+    {
+        StartCoroutine(DebugSpawnLoop());
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+#if UNITY_EDITOR
+    #region DEBUG
+
+    public GameObject DebugSpawn;
+    public Vector3 SpawnPoint;
+    private IEnumerator DebugSpawnLoop()
+    {
+        while(true)
+        {
+            TriggerBlockGeneration();
+            SpawnPoint += new Vector3(1F, 0F, 0F);
+            yield return new WaitForSeconds(0.5F);
+        }
+    }
+    #endregion
+#endif
+
+    #region
+    /// <summary>
+    /// Ajoute un block à la suite du niveua
+    /// </summary>
+    private void AddLevelBlock()
+    {
+        // TODO
+        var spawn = (GameObject)GameObject.Instantiate(DebugSpawn, SpawnPoint, Quaternion.identity);
+        var r = 255F / (float)Random.Range(0, 255);
+        var v = 255F / (float)Random.Range(0, 255);
+        var b = 255F / (float)Random.Range(0, 255);
+        spawn.GetComponent<MeshRenderer>().material.color = new Color(r, v, b);
+    }
+    #endregion
+
+    #region Evenements
+    /// <summary>
+    /// Lance la génération d'un nouveau block à la suite du parcourt
+    /// </summary>
+    public void TriggerBlockGeneration()
+    {
+        // TODO
+        AddLevelBlock();
+    }
+
+
+    #endregion
+}
