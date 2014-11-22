7df9176bc887c3ddd936f5f37e191fa2c4e4d5fb
unknown <bellecour@D24-45.bellecour.local>
2014-11-22 20:12:42 +0100

    boss

---
+++ Assets/Scripts/BigPea.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigPea : MonoBehaviour
+{
+    public float OscillationSpeed;
+    public float OscillationAmplitude;
+
+    private float startY;
+    void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    void Update()
+    {
+        var pos = transform.position;
+        pos.y = startY + Mathf.Sin(Time.time * OscillationSpeed) * OscillationAmplitude;
+        transform.position = pos;
+    }
+}
