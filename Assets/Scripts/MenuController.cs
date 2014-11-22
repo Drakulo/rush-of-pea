8ac9dc10eded9df52ab8da5d9d998266ed1e06b0
unknown <bellecour@D24-45.bellecour.local>
2014-11-22 21:31:12 +0100

    ajustements

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+    public void OnTapRestart()
+    {
+        Application.LoadLevel("Play");
+    }
+
+    public void OnTapQuit()
+    {
+        Application.Quit();
+    }
+}
