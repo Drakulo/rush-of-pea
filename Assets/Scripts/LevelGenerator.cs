8ca0fcb65bf1589ffbca6de8dc9bb1c4a83d0ab7
unknown <bellecour@D24-45.bellecour.local>
2014-11-22 11:42:08 +0100

    playmaker

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,6 +7,14 @@ public class LevelGenerator : MonoBehaviour
     // Nombre de blovks à générer d'avance
     public int ActiveMeshesCount;
 
+    // Distance de détection pour le spawn des blocks
+    public float SpawnCheckDistance;
+
+    // Blocks
+    public GameObject[] Blocks;
+
+    // Point de spawn actuel
+    public Vector3 SpawnPoint;
 
     private Queue<GameObject> _activeBlocks;
 
@@ -14,25 +22,48 @@ public class LevelGenerator : MonoBehaviour
     void Start()
     {
         _activeBlocks = new Queue<GameObject>();
-        StartCoroutine(DebugSpawnLoop());
+
+        // Génération des blocs de base
+        for (var i = 0; i < ActiveMeshesCount; i++ )
+        {
+            AddLevelBlock();
+        }
+
+        //StartCoroutine(DebugSpawnLoop());
     }
 
     void Update()
     {
+        // Envoi d'un raycast pour savoit s'il faut générer un nouveau block
+        var rayhit = new RaycastHit();
+     
+        //  
+        var origin = Camera.main.transform.position;
+        origin.z -= SpawnCheckDistance;
+        
+        //
+        var direction = new Vector3(0, -10, 0);
 
+        var ray = new Ray(origin, direction);
+
+        Debug.DrawRay(origin, direction, Color.red);
+
+        Physics.Raycast(ray, out rayhit);
+        if(rayhit.collider == null)
+        {
+            AddLevelBlock();
+        }
     }
     #endregion
 
 #if UNITY_EDITOR
     #region DEBUG
 
-    public GameObject DebugSpawn;
-    public Vector3 SpawnPoint;
     private IEnumerator DebugSpawnLoop()
     {
         while(true)
         {
-            TriggerBlockGeneration();
+            AddLevelBlock();
             yield return new WaitForSeconds(0.1F);
         }
     }
@@ -45,12 +76,14 @@ public class LevelGenerator : MonoBehaviour
     /// </summary>
     private void AddLevelBlock()
     {
-        // TODO
-        var block = (GameObject)GameObject.Instantiate(DebugSpawn, SpawnPoint, Quaternion.identity);
+        // Block aleatoire
+        var index = Random.Range(0, Blocks.Length);
+
+        var block = (GameObject)GameObject.Instantiate(Blocks[index], SpawnPoint, Quaternion.identity);
         block.transform.parent = transform;
         _activeBlocks.Enqueue(block);
 
-        SpawnPoint += new Vector3(0F, 0F, 6F);
+        SpawnPoint += new Vector3(0F, 0F, -6F);
 
         // Suppression des blocks inutiles
         if (ActiveMeshesCount < _activeBlocks.Count)
@@ -60,18 +93,5 @@ public class LevelGenerator : MonoBehaviour
             Destroy(oldBlock);
         }
     }
-    #endregion
-
-    #region Evenements
-    /// <summary>
-    /// Lance la génération d'un nouveau block à la suite du parcourt
-    /// </summary>
-    public void TriggerBlockGeneration()
-    {
-        // TODO
-        AddLevelBlock();
-    }
-
-
     #endregion
 }
